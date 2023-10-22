fab6497e825af284f206f2807c0699a68f743490
Edouard Outrebon <edoubn@gmail.com>
2023-10-22 17:40:57 +0200

    [CPU] Fixed several instructions

--- src/launch.rs
+++ src/launch.rs
@@ -1,14 +1,22 @@
 use std::fs::File;
-use std::io::{Read, Result};
-fn launch() -> Vec<u8> {
+use std::io::Read;
+use std::path::Path;
+
+pub(crate) fn launch() -> [u8;0xFFFF] {
 // Open the file for reading
-    let file_path = "snake.gb";
-    let mut file = File::open(file_path)?;
+
+    let path = Path::new("snake.gb");
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
+    let _ = file.read_to_end(&mut buffer);
+    
     let mut rom: Vec<u8> = Vec::new();
     //buffer = buffer[100..].to_vec();
-    for (index,byte) in buffer.iter().enumerate() {
+    for (_index,byte) in buffer.iter().enumerate() {
         let hex_value = format!("{:04X}", byte);
         if hex_value.len() == 2 {
             rom.push(0x00); // Add a leading zero for 1-byte values
@@ -23,6 +31,7 @@ fn launch() -> Vec<u8> {
     } else {
         println!("The Vec<u8> is empty.");
     }
-*/
-  rom
+*/  rom.try_into()
+    .unwrap_or_else(|rom: Vec<u8>| panic!("Expected a Vec of length {} but it was {}", 0xFFFF, rom.len()))
+
 }
