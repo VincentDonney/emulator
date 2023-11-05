4fe62cfb7006fcb8bca23e7b2c47c89b22ef886d
VincentDonney <donneyvinc@gmail.com>
2023-11-05 12:38:14 +0100

    function render_screen added

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,4 +1,4 @@
-use minifb::{Key, Window};
+use minifb::{Key, Window, WindowOptions};
 use crate::HEIGHT;
 use crate::WIDTH;
 use std::time::{Duration, Instant};
@@ -6,35 +6,30 @@ use std::time::{Duration, Instant};
 
 
 
-pub fn window_run(mut window: Window, mut buffer: Vec<u32>) {
+pub fn render_screen(mut window: Window, mut buffer: Vec<u32>, mut video_buffer: [u8;160*144]) {
 
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600))); // ~60fps
 
-    let mut x: usize = 0;
-    let mut y: usize = 0;
 
     let mut fps_timer = Instant::now();
     let mut fps_counter = 0;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
 
-
-        // Move the pixel when the arrow keys are pressed
-        if window.is_key_down(Key::Left) && x > 0 {
-            x -= 1;
+        //loop through buffer to change the pixels color
+        let mut i = 0;
+        while i < buffer.len() {
+            let mut color_value = 0x00FFFFFF;   //default value
+            match video_buffer[i] {
+                0 => color_value = 0x00FFFFFF,  //white pixel
+                1 => color_value = 0x00A9A9A9,  //light gray pixel
+                2 => color_value = 0x00545454,  //dark gray pixel
+                3 => color_value = 0x00000000,  //black pixel
+                _ => println!("Wrong pixel value while updating the buffer\n"),
+            }
+            buffer[i] = color_value;
+            i = i+1;
         }
-        if window.is_key_down(Key::Right) && x < WIDTH - 1 {
-            x += 1;
-        }
-        if window.is_key_down(Key::Up) && y > 0 {
-            y -= 1;
-        }
-        if window.is_key_down(Key::Down) && y < HEIGHT - 1 {
-            y += 1;
-        }
-
-        // Set the pixel to blue
-        buffer[y * WIDTH + x] = 0x0000FFFF;
 
         // Calculate fps
         fps_counter += 1;
@@ -46,6 +41,7 @@ pub fn window_run(mut window: Window, mut buffer: Vec<u32>) {
             fps_timer = Instant::now();
         }
 
+        
         // Update the window buffer and display the changes
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
